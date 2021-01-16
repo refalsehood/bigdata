efe41728a9c611af5ebb4a70dd5937e98746bb94
tangsan <462068393@qq.com>
2021-01-16 18:50:16 +0800

    唐门建立

---
+++ src/main/java/com/pure/haotian/TangMenAnQi.java
@@ -0,0 +1,8 @@
+package com.pure.haotian;
+
+public class TangMenAnQi {
+    public static void main(String[] args) {
+        System.out.println("佛怒莲花");
+        System.out.println("二十四桥明月夜");
+    }
+}
