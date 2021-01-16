dac69645d22e41ed694c1175847b32031f0d9882
tansan <462068393@qq.com>
2021-01-16 15:09:38 +0800

    2.昊天锤的基础学习

--- src/main/java/com/pure/haotian/HaoTianChuiFa.java
+++ src/main/java/com/pure/haotian/HaoTianChuiFa.java
@@ -7,16 +7,20 @@ public class HaoTianChuiFa {
         System.out.println("昊天锤的基础功法3-唐昊");
         System.out.println("昊天锤的基础功法4-唐昊");
         System.out.println("昊天锤的基础功法5-唐昊");
-        System.out.println("===================");
-        System.out.println("昊天锤的进阶功法1-唐昊");
-        System.out.println("昊天锤的进阶功法2-唐昊");
-        System.out.println("昊天锤的进阶功法3-唐昊");
-        System.out.println("昊天锤的进阶功法4-唐昊");
-        System.out.println("昊天锤的进阶功法5-唐昊");
+
         System.out.println("昊天锤的基础功法1-唐三");
         System.out.println("昊天锤的基础功法2-唐三");
         System.out.println("昊天锤的基础功法3-唐三");
         System.out.println("昊天锤的基础功法4-唐三");
         System.out.println("昊天锤的基础功法5-唐三");
+
+        System.out.println("===================");
+
+        System.out.println("昊天锤的进阶功法1-唐昊");
+        System.out.println("昊天锤的进阶功法2-唐昊");
+        System.out.println("昊天锤的进阶功法3-唐昊");
+        System.out.println("昊天锤的进阶功法4-唐昊");
+        System.out.println("昊天锤的进阶功法5-唐昊");
+
     }
 }
