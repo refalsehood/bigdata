c1e0a8c8c2d5dbf401f0fe868f8ce2d8993635d6
pure <462068393@qq.com>
2021-01-16 13:39:08 +0800

    1.初始教学

---
+++ src/main/java/com/pure/haotian/HaoTianChuiFa.java
@@ -0,0 +1,12 @@
+package com.pure.haotian;
+
+public class HaoTianChuiFa {
+    public static void main(String[] args) {
+        System.out.println("昊天锤的基础功法1-唐昊");
+        System.out.println("昊天锤的基础功法2-唐昊");
+        System.out.println("昊天锤的基础功法3-唐昊");
+        System.out.println("昊天锤的基础功法4-唐昊");
+        System.out.println("昊天锤的基础功法5-唐昊");
+        System.out.println("===================");
+    }
+}
