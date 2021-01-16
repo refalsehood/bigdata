9a8c532cbac4e8da2f8ec676abe9315a1537b7cd
tanhao <18686986095@163.com>
2021-01-16 13:56:41 +0800

    1.进阶教学

--- src/main/java/com/pure/haotian/HaoTianChuiFa.java
+++ src/main/java/com/pure/haotian/HaoTianChuiFa.java
@@ -8,5 +8,10 @@ public class HaoTianChuiFa {
         System.out.println("昊天锤的基础功法4-唐昊");
         System.out.println("昊天锤的基础功法5-唐昊");
         System.out.println("===================");
+        System.out.println("昊天锤的进阶功法1-唐昊");
+        System.out.println("昊天锤的进阶功法2-唐昊");
+        System.out.println("昊天锤的进阶功法3-唐昊");
+        System.out.println("昊天锤的进阶功法4-唐昊");
+        System.out.println("昊天锤的进阶功法5-唐昊");
     }
 }
